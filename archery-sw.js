cea60afe292e22a75ce3e5df5c0fa622aa1fe116
bsk65 <bsklausen@proton.me>
2026-05-01 07:58:28 +0200

    Delete archery-sw.js

--- archery-sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='3d-archery-v100';
-const FILES=['./archery-app.html'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));self.clients.claim();});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).catch(function(){return caches.match('./archery-app.html');});}));});
