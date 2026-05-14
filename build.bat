842ff23f558a0b16d74c1f4b31ef5fcea497d9b0
Bjarne Klausen <bkl@odensezoo.dk>
2026-05-14 08:32:31 +0200

    14 maj

--- build.bat
+++ build.bat
@@ -1,17 +1,17 @@
 @echo off
 echo === 3D Bueskydning Build ===
 
-REM Gem kilde index.html
-copy index.html index.src.html /Y >nul
+REM Brug altid index.src.html som kilde til Vite
+copy index.src.html index.html /Y >nul 2>&1
 
-REM Byg med Vite
+REM Byg med Vite (bruger nu korrekt index.html)
 call npm run build
 
 REM Kopier assets
 xcopy dist\assets\* assets\ /E /Y /Q
 
-REM Gendan kilde index.html (VIGTIGT - ikke dist versionen)
-copy index.src.html index.html /Y >nul
+REM Erstat index.html med den BYGGEDE version
+copy dist\index.html index.html /Y >nul
 
 echo === Faerdig - push nu i GitHub Desktop ===
 pause
